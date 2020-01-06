02beffdd91da5e711cc3ac9ae9688c7c31f4c1f0
Romero <lucasmfromero@gmail.com>
2020-01-05 21:35:55 -0300

    Add deleted header files

---
+++ header/Screen.h
@@ -0,0 +1,14 @@
+#ifndef SCREEN_H
+#define SCREEN_H
+
+#include <string>
+using namespace std;
+
+class Screen {
+	public:
+		void displayMessage(string);
+		void displayLineMessage(string);
+};
+
+
+#endif
